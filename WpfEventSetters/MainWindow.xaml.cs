c202e7b63506001545a963726188d27ae2a02abb
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2019-12-10 18:15:16 +0300

    Add project files.

---
+++ WpfEventSetters/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfEventSetters
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_MouseEnter(object sender, RoutedEventArgs e)
+		{
+			Button btn = (Button)sender;
+			int value;
+			if (int.TryParse((string)btn.Content, out value)){ //out value . out keyword allow to null value for value type variables
+					value++;
+					btn.Content.ToString();
+				}
+				}
+
+				}
+				}
+
+
+
+
+
+
---
+++ WpfOdev_Hafta_10/ShowTime.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace WpfOdev_Hafta_10
+{
+	public class ShowTime:MarkupExtension
+	{
+		private string _metin = string.Empty;
+
+		public string Metin { get => _metin; set => _metin = value; }
+		public ShowTime()
+		{
+
+		}
+		public ShowTime(string input)
+		{
+		_metin = input;
+		}
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+
+			return string.Format("{0} : {1}",Metin, DateTime.Now.ToString("dd MMMM yyyy , dddd"));
+			#region C# DateTime Fomats
+			/*
+			 * Format	Result
+DateTime.Now.ToString("MM/dd/yyyy")	05/29/2015
+DateTime.Now.ToString("dddd, dd MMMM yyyy")	Friday, 29 May 2015
+DateTime.Now.ToString("dddd, dd MMMM yyyy")	Friday, 29 May 2015 05:50
+DateTime.Now.ToString("dddd, dd MMMM yyyy")	Friday, 29 May 2015 05:50 AM
+DateTime.Now.ToString("dddd, dd MMMM yyyy")	Friday, 29 May 2015 5:50
+DateTime.Now.ToString("dddd, dd MMMM yyyy")	Friday, 29 May 2015 5:50 AM
+DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss")	Friday, 29 May 2015 05:50:06
+DateTime.Now.ToString("MM/dd/yyyy HH:mm")	05/29/2015 05:50
+DateTime.Now.ToString("MM/dd/yyyy hh:mm tt")	05/29/2015 05:50 AM
+DateTime.Now.ToString("MM/dd/yyyy H:mm")	05/29/2015 5:50
+DateTime.Now.ToString("MM/dd/yyyy h:mm tt")	05/29/2015 5:50 AM
+DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")	05/29/2015 05:50:06
+DateTime.Now.ToString("MMMM dd")	May 29
+DateTime.Now.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss.fffffffK")	2015-05-16T05:50:06.7199222-04:00
+DateTime.Now.ToString("ddd, dd MMM yyy HH’:’mm’:’ss ‘GMT’")	Fri, 16 May 2015 05:50:06 GMT
+DateTime.Now.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss")	2015-05-16T05:50:06
+DateTime.Now.ToString("HH:mm")	05:50
+DateTime.Now.ToString("hh:mm tt")	05:50 AM
+DateTime.Now.ToString("H:mm")	5:50
+DateTime.Now.ToString("h:mm tt")	5:50 AM
+DateTime.Now.ToString("HH:mm:ss")	05:50:06
+DateTime.Now.ToString("yyyy MMMM")	2015 May
+				*/
+			#endregion
+		}
+
+		//return string.Format("{0}: {1}", 		header, DateTime.Now.ToLongTimeString());
+		//public override string ToString()
+		//{
+		//	return string.Format("0}",DateTime.Now.ToLongTimeString());
+		//}
+	}
+}
